151144176af18f693298d6529c5b11a9245022bd
natchez <natchezzz@hotmail.com>
2021-03-18 01:23:01 +0100

    fin header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Title from './Title';
 
 function Header(props) {
     return (
+        <header>
+            <div className="barre-gauche">
+                <div className="logo">
+                <h6>Natchez</h6>
+                <h6 className="return">REnders</h6>
+                </div>
+                <div className="nav-link">
+                    <ul>
+                        <li>< NavLink to='./' exact activeClassName="nav-active">Home</NavLink></li>
+                        <li>< NavLink to='./projet' exact activeClassName="nav-active">projet</NavLink></li>
+                        <li>< NavLink to='./contact' exact activeClassName="nav-active">contact</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="barre-droite">
+            <ul>
+                <li><a href="https://www.instagram.com/?hl=fr" target='blank'>Instagram</a></li>
+                <li><a href="https://www.facebook.com/" target='blank'>Facebook</a></li>
+                <li><a href="https://www.behance.net/" target='blank'>Behance</a></li>
+                <li><a href="https://github.com/" target='blank'>Github</a></li>
+            </ul>
+            </div>
+            <Title/>
+        </header>
     );
 }
 
---
+++ src/components/header/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Title(props) {
+    return (
+        <div className="header-title">
+            <h1>Natchez</h1>
+            <hr/>
+            <h1 className="return">Renders</h1>
+        </div>
+    );
+}
+
+export default Title;
